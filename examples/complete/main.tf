875d5477e373b5df6674b0465d80051d1aa75e2e
Teddy Suyanto <tekka171@yahoo.com>
2019-12-27 17:32:46 +0700

    add complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,9 +1,15 @@
-module "api" {
-  source         = "../../"
+locals {
   product_domain = "BEI"
-  service        = "beical"
   cluster        = "beical-app"
   environment    = "production"
+}
+
+module "api" {
+  source         = "../../"
+  product_domain = "${local.product_domain}"
+  service        = "beical"
+  cluster        = "${local.cluster}"
+  environment    = "${local.production}"
   tags           = ["tag1", "tag2"]
 
   recipients         = ["bei@traveloka.com"]
@@ -12,6 +18,7 @@ module "api" {
   renotify_interval  = 0
   notify_audit       = false
 
+  latency_p95_name = "${local.product_domain} - ${local.cluster} - ${local.environment} - API Latency is High"
   latency_p95_thresholds = {
     critical = 1000
     warning  = 800
@@ -19,7 +26,9 @@ module "api" {
 
   latency_p95_message            = "Monitor is triggered"
   latency_p95_escalation_message = "Monitor isn't resolved for given interval"
+  latency_p95_include_tags       = false
 
+  exception_name = "${local.product_domain} - ${local.cluster} - ${local.environment} - API Exception is High"
   exception_thresholds = {
     critical = 50
     warning  = 20
@@ -27,4 +36,5 @@ module "api" {
 
   exception_message            = "Monitor is triggered"
   exception_escalation_message = "Monitor isn't resolved for given interval"
+  exception_include_tags       = false
 }
